1c50b3eb100d8b97c79667e19a1fa675d14ea807
wellingtonacosta <wellingtonacosta09@gmail.com>
2023-05-26 21:34:03 -0300

    feature: reset do app

---
+++ MED Notify/src/pages/LoginScreen.tsx
@@ -0,0 +1,16 @@
+export const LoginScreen  = () => {
+  return (
+    <>
+    <div> 
+      Container Page
+      <div>Container de Login
+        <div>
+          Container Input
+        </div>
+      </div>
+      <img src="" alt="" />
+    </div>
+
+    </>
+  )
+}
